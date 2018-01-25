189ba868b968210539b27f685b7f9450260e5618
carlos <CarSanoja@wuopcarlos@gmail.com>
2018-01-24 22:07:28 -0400

    Change the angle to turn when detect and obstacle Tunning the threshold amgles to know when stop and continue

--- code/drive_rover.py
+++ code/drive_rover.py
@@ -59,7 +59,7 @@ class RoverState():
         # of navigable terrain pixels.  This is a very crude form of knowing
         # when you can keep going and when you should stop.  Feel free to
         # get creative in adding new fields or modifying these!
-        self.stop_forward = 50 # Threshold to initiate stopping
+        self.stop_forward = 200 # Threshold to initiate stopping
         self.go_forward = 500 # Threshold to go forward again
         self.max_vel = 2 # Maximum velocity (meters/second)
         # Image output from perception step
@@ -77,6 +77,8 @@ class RoverState():
         self.near_sample = 0 # Will be set to telemetry value data["near_sample"]
         self.picking_up = 0 # Will be set to telemetry value data["picking_up"]
         self.send_pickup = False # Set to True to trigger rock pickup
+        self.rock_angle = None # Angulo de los pixeles de las piedras
+        self.rock_dist = None # DDistancias de los pixeles de las piedras
 # Initialize our rover 
 Rover = RoverState()
 
